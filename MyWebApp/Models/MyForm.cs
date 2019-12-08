a6acd092839c6c35f21e89bffd3317197a0418ef
tsubakimoto <tkmbmbr@gmail.com>
2019-12-08 17:35:55 +0900

    Add message label

--- MyWebApp/Models/MyForm.cs
+++ MyWebApp/Models/MyForm.cs
@@ -1,7 +1,10 @@
-﻿namespace MyWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApp.Models
 {
     public class MyForm
     {
+        [Display(Name = "メッセージ")]
         public string Message { get; set; }
     }
 }
